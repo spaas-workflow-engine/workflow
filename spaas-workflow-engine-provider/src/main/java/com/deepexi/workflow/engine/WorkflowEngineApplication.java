c5c470095cca1e578811aaf5fb9db6832a43d9e4
chenling <chenling@deepexi.com>
2019-10-26 11:47:26 +0800

    项目初始化

---
+++ spaas-workflow-engine-provider/src/main/java/com/deepexi/workflow/engine/WorkflowEngineApplication.java
@@ -0,0 +1,13 @@
+package com.deepexi.workflow.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowEngineApplication.class, args);
+    }
+
+}
---
+++ spaas-workflow-engine-provider/src/main/java/com/deepexi/workflow/engine/WorkflowEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepexi.workflow.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
